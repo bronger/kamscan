f9546f69b4d2948f24cd0b2bd73775fa7222d668
Torsten Bronger <bronger@physik.rwth-aachen.de>
2017-07-19 08:04:38 +0200

    Finalized coordinate extraction.

--- analyze_scan.py
+++ analyze_scan.py
@@ -14,20 +14,26 @@ class Result(Exception):
 
 class ImageWindow(Image):
 
-    def __init__(self, x, y, scaling, *args, **kwars):
-        super().__init__(*args, **kwars)
-        # FixMe: Make use of x, y, and scaling
-        ...
-        file_width, file_height = subprocess.check_output(["identify", self.source]).decode().split()[2].split("x")
-        self.file_width, self.file_height = int(file_width), int(file_height)
+    def __init__(self, x, y, scaling, source, *args, **kwargs):
+        raw_width, raw_height = subprocess.check_output(["identify", source]).decode().split()[2].split("x")
+        self.crop_width = min(int(raw_width), 1100 / scaling)
+        self.crop_height = min(int(raw_height), 900 / scaling)
+        self.x0 = x - self.crop_width / 2
+        self.y0 = y - self.crop_height / 2
+        subprocess.check_call(["convert", "-extract", "{}x{}+{}+{}".format(self.crop_width, self.crop_height,
+                                                                           self.x0, self.y0), source, "+repage",
+                               "-resize", "{}%".format(scaling * 100), "/tmp/analyze_scan.ppm"])
+        self.scaling = scaling
+        kwargs["source"] = "/tmp/analyze_scan.ppm"
+        super().__init__(*args, **kwargs)
 
     def on_touch_down(self, touch):
         image_width, image_height = self.norm_image_size
         offset_x = (self.width - image_width) / 2
         offset_y = (self.height - image_height) / 2
-        x = (touch.x - offset_x) * self.file_width / image_width
-        y = self.file_height - (touch.y - offset_y) * self.file_height / image_height
-        raise Result(x, y)
+        x = (touch.x - offset_x) * self.crop_width / image_width
+        y = self.crop_height - (touch.y - offset_y) * self.crop_height / image_height
+        raise Result(x + self.x0, y + self.y0)
 
 
 class AnalyzeApp(App):
@@ -40,7 +46,7 @@ class AnalyzeApp(App):
         super().__init__(*args, **kwargs)
 
     def build(self):
-        return ImageWindow(self.x, self.y, self.scaling, source=self.source)
+        return ImageWindow(self.x, self.y, self.scaling, self.source)
 
     def run(self):
         try:
