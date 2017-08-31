71ea88bbba46dfc6e248c171c16404d2831e4855
Torsten Bronger <bronger@physik.rwth-aachen.de>
2017-08-31 13:49:30 +0200

    Made mocking script for general.
    It can now feed the content of the directory /tmp/kamscan_mock into Kamscan.

--- mock.sh
+++ mock.sh
@@ -2,8 +2,8 @@
 set -e
 rm -Rf /tmp/kamscan
 mkdir -p /tmp/kamscan/3937-6637/DCIM
-if [ "$1" != "empty" ]
+if [ "$1" != "--empty" ]
 then
-    cp /home/bronger/src/kamscan/DSC06499.ARW /tmp/kamscan/3937-6637/DCIM
+    cp /tmp/kamscan_mock/*.ARW /tmp/kamscan/3937-6637/DCIM
 fi
 sudo mv /tmp/kamscan/3937-6637 /media/bronger
