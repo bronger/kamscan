18737434ee2d51f211a1eb183e1c2dd4172265bf
Torsten Bronger <bronger@physik.rwth-aachen.de>
2017-09-11 20:34:53 +0200

    Made rsync call into a comment.
    It happens in the wrong moment otherwise.

--- complete_mock.sh
+++ complete_mock.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
-rsync -a --delete ~/temp/kamscan_mock/profiles/default/ ~/.config/kamscan/profiles/default/
+# Please install the profile first with:
+#
+# rsync -r --delete ~/temp/kamscan_mock/profiles/default/ ~/.config/kamscan/profiles/default/
+
 ./unmock.sh ; sleep 3
 ./mock.sh $1 0 ; sleep 3
 ./unmock.sh ; sleep 3
