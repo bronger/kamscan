57bc92fa4b15be3857112c3edd14b4190b49e0a6
Torsten Bronger <bronger@physik.rwth-aachen.de>
2018-12-29 09:21:58 +0100

    Bumped up Python version.

--- install_undistort.sh
+++ install_undistort.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-sudo rm /usr/local/lib/python3.5/dist-packages/undistort* || true
+sudo rm /usr/local/lib/python3.6/dist-packages/undistort* || true
 cd ~/src/kamscan
 sudo rm -Rf build
 ./setup.py build && sudo ./setup.py install
