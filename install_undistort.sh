fe10bd737d07b933e17d0cd29215714026489367
Torsten Bronger <bronger@physik.rwth-aachen.de>
2018-12-29 09:21:38 +0100

    Made script sensitive to errors.

--- install_undistort.sh
+++ install_undistort.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+set -e
 
-sudo rm /usr/local/lib/python3.5/dist-packages/undistort*
-sudo rm /usr/local/lib/python3.5/dist-packages/undistort.cpython-35m-x86_64-linux-gnu.so
+sudo rm /usr/local/lib/python3.5/dist-packages/undistort* || true
 cd ~/src/kamscan
 sudo rm -Rf build
-./setup.py build && sudo ./setup.py install && python3 -c "import undistort"
+./setup.py build && sudo ./setup.py install
+python3 -c "import undistort"
