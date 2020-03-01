6fefc126d488e04c25035153207717affe9fa1d3
Torsten Bronger <bronger@physik.rwth-aachen.de>
2020-03-01 12:06:10 +0100

    Removed obsolete code.

--- setup.py
+++ setup.py
@@ -4,9 +4,7 @@ from distutils.core import setup, Extension
 
 undistort = Extension("undistort",
                       sources=["undistort.cc"],
-                      libraries=["lensfun"],
-                      # Only necessary before Ubuntu 17.04.
-                      extra_compile_args=["-std=c++11"])
+                      libraries=["lensfun"])
 
 setup(name="undistort",
       ext_modules=[undistort],
