4c89ac3c5f6c28fdd45f3916ac3416ae15277fc2
Torsten Bronger <bronger@physik.rwth-aachen.de>
2023-05-16 08:18:14 +0200

    Move silent_call into its own utils package
    This way, it can also be used by the different sources.

--- kamscan.py
+++ kamscan.py
@@ -13,6 +13,8 @@ from pathlib import Path
 import pytz, argcomplete
 from ruamel.yaml import YAML
 import undistort
+from kamscan import utils
+from kamscan.utils import silent_call
 
 
 yaml = YAML()
@@ -50,6 +52,8 @@ parser.add_argument("filepath", type=Path, help="path to the PDF file for storin
                     "YYYY-MM-DD_Title")
 argcomplete.autocomplete(parser)
 args = parser.parse_args()
+
+utils.debug = args.debug
 
 assert "/" not in args.profile
 profile_root = profiles_root/args.profile
@@ -153,39 +157,6 @@ def datetime_to_pdf(timestamp, timestamp_accuracy="full"):
         timestamp = timestamp.strftime("D:%Y%m%d%H%M%S%z")
         timestamp = "{}'{}'".format(timestamp[:-2], timestamp[-2:])
     return timestamp
-
-
-def silent_call(arguments, asynchronous=False, swallow_stdout=True):
-    """Calls an external program.  stdout and stderr are swallowed by default.  The
-    environment variable ``OMP_THREAD_LIMIT`` is set to one, because we do
-    parallelism by ourselves.  In particular, Tesseract scales *very* badly (at
-    least, version 4.0) with more threads.
-
-    :param list[object] arguments: the arguments for the call.  They are
-      converted to ``str`` implicitly.
-    :param bool asynchronous: whether the program should be launched
-      asynchronously
-    :param bool swallow_stdout: if ``False``, stdout is caught and can be
-      inspected by the caller (as a str rather than a byte string)
-
-    :returns: if asynchronous, it returns a ``Popen`` object, otherwise, it
-      returns a ``CompletedProcess`` object.
-    :rtype: subprocess.Popen or subprocess.CompletedProcess
-
-    :raises subprocess.CalledProcessError: if a synchronously called process
-      returns a non-zero return code
-    """
-    environment = os.environ.copy()
-    environment["OMP_THREAD_LIMIT"] = "1"
-    kwargs = {"stdout": subprocess.DEVNULL if swallow_stdout else subprocess.PIPE,
-              "stderr": None if args.debug else subprocess.DEVNULL,
-              "universal_newlines": True, "env": environment}
-    arguments = list(map(str, arguments))
-    if asynchronous:
-        return subprocess.Popen(arguments, **kwargs)
-    else:
-        kwargs["check"] = True
-        return subprocess.run(arguments, **kwargs)
 
 
 class Camera:
---
+++ utils.py
@@ -0,0 +1,37 @@
+import subprocess, os
+
+
+debug = False
+
+
+def silent_call(arguments, asynchronous=False, swallow_stdout=True):
+    """Calls an external program.  stdout and stderr are swallowed by default.  The
+    environment variable ``OMP_THREAD_LIMIT`` is set to one, because we do
+    parallelism by ourselves.  In particular, Tesseract scales *very* badly (at
+    least, version 4.0) with more threads.
+
+    :param list[object] arguments: the arguments for the call.  They are
+      converted to ``str`` implicitly.
+    :param bool asynchronous: whether the program should be launched
+      asynchronously
+    :param bool swallow_stdout: if ``False``, stdout is caught and can be
+      inspected by the caller (as a str rather than a byte string)
+
+    :returns: if asynchronous, it returns a ``Popen`` object, otherwise, it
+      returns a ``CompletedProcess`` object.
+    :rtype: subprocess.Popen or subprocess.CompletedProcess
+
+    :raises subprocess.CalledProcessError: if a synchronously called process
+      returns a non-zero return code
+    """
+    environment = os.environ.copy()
+    environment["OMP_THREAD_LIMIT"] = "1"
+    kwargs = {"stdout": subprocess.DEVNULL if swallow_stdout else subprocess.PIPE,
+              "stderr": None if debug else subprocess.DEVNULL,
+              "universal_newlines": True, "env": environment}
+    arguments = list(map(str, arguments))
+    if asynchronous:
+        return subprocess.Popen(arguments, **kwargs)
+    else:
+        kwargs["check"] = True
+        return subprocess.run(arguments, **kwargs)
